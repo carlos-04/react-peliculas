dd60dd47edae9a50689060d412fd13939723198b
carlos-04 <monterocarlos680@gmail.com>
2021-08-08 22:44:40 -0400

    version-2

--- src/components/generos/IndiceGeneros.tsx
+++ src/components/generos/IndiceGeneros.tsx
@@ -1,6 +1,18 @@
+import axios, { AxiosResponse } from 'axios';
+import { useEffect } from 'react';
 import {NavLink} from 'react-router-dom';
+import { generoDTO } from '../../models/Genero.model';
+import { urlGeneros } from '../../services/endpoint';
 
 const IndiceGeneros = () => {
+
+useEffect(() => {
+axios.get(urlGeneros)
+     .then((respuesta : AxiosResponse<generoDTO[]>) => {
+            console.log(respuesta.data)
+     })
+}, [])
+
     return (
         <div>
              <h1>Indice Genero</h1> 
